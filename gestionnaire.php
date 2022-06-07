5e5b0d6a540dfa07b6ca9ceebeca823a64655a68
sidy8202 <diakitesidy3@gmail.com>
2022-06-07 17:02:00 +0200

    First version

---
+++ gestionnaire.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="assets/css/gestionnaire.css">
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+    <title>Gestionnaire</title>
+</head>
+    <body style="background-color:#0d4f9b;">
+        
+        <div class="mx-4">
+            <div class="row mt-3">
+                <div class="col-md-2 col-sm gauche border">
+                    <div class="devdiagui">
+                        <img src="ok.jpeg" alt="" srcset="" class="haaou">   
+                        <p>Mme Ouatt Maria Dao</p>
+                        <p>Gestionnaire</p>
+                    </div>
+                </div>
+
+                <div class="col-md sanata border mx-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col border">
+                                <div class="xol-sm col-md col-xl">
+                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores quia doloremque nihil repellat totam. Minus veniam, consectetur quod autem quaerat vitae quis illo voluptates odit necessitatibus! Exercitationem nobis iusto rerum?
+                                    Doloribus excepturi, quibusdam eum quisquam qui vitae minima praesentium, architecto sit incidunt quidem quos magni quo mollitia labore? Adipisci exercitationem quibusdam voluptates iste magnam, est rerum reprehenderit esse aut ab?
+                                    Aliquam alias cumque rem quibusdam voluptates harum consequuntur nisi? Consequuntur, ut repellat. Beatae debitis aliquid nam tempore, commodi qui exercitationem voluptas assumenda saepe deleniti itaque facilis, harum eaque illo fugiat?
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row border mt-3 haoua">
+                            <div class="col-xl col-md col-sm gest">
+                                <p class="para mt-5">20</p>
+                                <p class="graphe">Memres</p>
+                            </div>
+
+                            <div class="col-xl col-md col-sm membr">
+                                <p class="col-xl para mt-5">12</p>
+                                <p class="graphe">Cotisation</p>
+                            </div>
+                            <div class="col-xl co-mdl col-sm coti">
+                                <p class="para mt-5">2.000.000Fcfa</p>
+                                <p class="graphe">Mon solde</p>
+                            </div>
+                            <div class="col-xl col-md col-sm forum">
+                                <p class="para mt-5">5.000.000Fcfa</p>
+                                <p class="graphe">Solde Tontine</p>
+                            </div>
+                        </div>
+                        <div class="row mt-3 border">
+                            <div class="col-xl col-sm col-md-8">
+                                <h3>A propos</h3>
+                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam consectetur tempore minus earum quisquam aperiam consequuntur at quae eos! Quis eveniet iste aperiam asperiores accusamus. Quia praesentium error aperiam ea!
+                                    Ipsum, ducimus deleniti sed quam voluptatibus asperiores explicabo, ea impedit ipsa aut, saepe sequi commodi sunt ipsam corporis molestiae odio eligendi necessitatibus! Eaque nam minus dolore repellendus enim! Tenetur, non!
+                                    Architecto eius vero omnis quisquam veritatis voluptatem quibusdam ea distinctio quis maiores possimus dolorem placeat ab optio provident assumenda mollitia delectus, enim esse in id at, atque excepturi dolor. Unde?</p>
+
+                            </div>
+                            <div class="col-xl col-sm col-md border">
+                                <h3>Calendrier</h3>
+                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, explicabo quasi deleniti blanditiis cumque quam ducimus sapiente necessitatibus rem recusandae enim maxime placeat et ad modi quisquam culpa, adipisci temporibus?</p>
+
+                            </div>
+                        </div>
+                    </div>        
+                </div>
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
